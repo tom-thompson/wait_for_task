aa9ac79d3dc10337fc61fc5e9da5848dd78f70f6
Tom Thompson <tom@codethulu.com>
2019-01-15 20:17:20 -0500

    wait for task

---
+++ Assets/Scripts/WaitForTask.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Codethulu
+{
+    public class WaitForTask : CustomYieldInstruction
+    {
+        private Task task;
+
+        public override bool keepWaiting {
+            get {
+                return !task.IsCompleted;
+            }
+        }
+
+        public WaitForTask(Task task) {
+            this.task = task;
+        }
+    }
+}
---
+++ Assets/Tests/WaitForTaskTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using Codethulu;
+
+namespace Tests
+{
+    public class WaitForTaskTest
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator WaitForTaskTestWithEnumeratorPasses()
+        {
+            int val = 0;
+
+            Task t = new Task(() =>
+            {
+                Thread.Sleep(1000);
+                val = 1;
+            });
+            t.Start();
+
+            yield return new WaitForTask(t);
+
+            Assert.AreEqual(1, val);
+        }
+    }
+}
